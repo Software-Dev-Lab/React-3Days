b74e5e7b0ea8ab50d570f3b00cfe16767557ec4b
9j <azer6666@163.com>
2024-03-31 00:49:50 +0800

    第六课： getSnapshotBeforeUpdate的特点

--- src/App.js
+++ src/App.js
@@ -3,52 +3,56 @@ import './App.css';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    
     this.state = {
-      name: 'hello'
+      name: '初始化'
     }
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log('props', props);
-    console.log('state', state);
-    console.log('getDerivedStateFromProps');
+    console.log('getDerivedStateFromProps 函数执行')
     return {
-      // name: '哈哈  我把你拦截了'
-    }
+    };
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('prevProps', prevProps);
-    console.log('prevState', prevState);
-    console.log('getSnapshotBeforeUpdate');
-    return null;
+    console.log('getSnapshotBeforeUpdate 函数执行')
+    return {
+      snapshot: '我传给你的信息'
+    };
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('prevProps', prevProps);
-    console.log('prevState', prevState);
-    console.log('snapshot', snapshot);
-    console.log('组件更新完成');
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('prevProps', prevProps)
+    console.log('prevState', prevState)
+    console.log('snapshot', snapshot)
+    console.log('componentDidUpdate 页面更新之后')
   }
 
+
+  // 组件挂载完成之后执行
   componentDidMount() {
-    console.log('组件挂载完成');
+    console.log('componentDidMount 组件挂载完成');
   }
 
+  // 定义事件
   click = () => {
+    // 修改状态
     this.setState({
-      name: 'change'
+      name: '修改后的name'
     })
   }
 
   render() {
+    console.log('render 函数执行')
     const { name } = this.state
-    console.log('render 执行')
     return (
       <div>
         <h1>{name}</h1>
-        <button onClick={this.click}>点我修改状态</button>
+        <button onClick={this.click}>
+          修改name
+        </button>
       </div>
     )
   }
