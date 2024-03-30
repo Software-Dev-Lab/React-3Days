72871521ec873cc5ca7979415a5f91e39bbd00b0
9j <azer6666@163.com>
2024-03-31 01:10:27 +0800

    第七课：卸载生命周期

--- src/App.js
+++ src/App.js
@@ -1,57 +1,45 @@
 import React from 'react';
 import './App.css';
 
+class Bpp extends React.Component {
+
+  componentWillUnmount() {
+    console.log('Bpp组件卸载')
+  }
+
+  render() {
+    return (
+      <h1>我是Bpp</h1>
+    )
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     
     this.state = {
-      name: '初始化'
+      bpp_show: true
     }
   }
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('getDerivedStateFromProps 函数执行')
-    return {
-    };
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforeUpdate 函数执行')
-    return {
-      snapshot: '我传给你的信息'
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot){
-    console.log('prevProps', prevProps)
-    console.log('prevState', prevState)
-    console.log('snapshot', snapshot)
-    console.log('componentDidUpdate 页面更新之后')
-  }
-
-
-  // 组件挂载完成之后执行
-  componentDidMount() {
-    console.log('componentDidMount 组件挂载完成');
-  }
-
   // 定义事件
   click = () => {
     // 修改状态
     this.setState({
-      name: '修改后的name'
+      bpp_show: false
     })
   }
 
   render() {
     console.log('render 函数执行')
-    const { name } = this.state
+    const { bpp_show } = this.state
     return (
       <div>
-        <h1>{name}</h1>
+        {
+          bpp_show && <Bpp />
+        }
         <button onClick={this.click}>
-          修改name
+          隐藏bpp
         </button>
       </div>
     )
