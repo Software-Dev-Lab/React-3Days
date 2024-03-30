3ea9d187de7a748e661b0a170954625d695167f7
9j <azer6666@163.com>
2024-03-30 18:03:45 +0800

    第三课： getDerivedStateFromProps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'hello'
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('props', props);
+    console.log('state', state);
+    console.log('getDerivedStateFromProps');
+    return {
+      // name: 'world'
+    }
+  }
+
+  componentDidMount() {
+    console.log('组件挂载完成');
+  }
+
+  render() {
+    const { name } = this.state
+
+    return (
+      <h1>{name}</h1>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // 性能分析相关，暂时不用管
