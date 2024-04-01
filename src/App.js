f1be6863a427f546f62c0ac531750caf73b4e2fe
9j <azer6666@163.com>
2024-04-01 23:28:46 +0800

    第九课： useEffect 02

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
 
+  const [count, setCount] = useState(0) // [0, function(){}
+  const [name, setName] = useState(() => {
+    return '张三'
+  }) // ['张三', function(){}
+
+  useEffect(() => {
+    console.log('空的useEffect')
+  })
+
   // 第一个参数回调函数：组件初始化执行，组件更新之后会执行（dom更新完成）
   useEffect(() => {
-    console.log('useEffect执行')
+    // console.log('name发生了变化', count)
+    // return () => {
+    //   console.log('组件卸载')
+    // }
+  },[name])
 
-    return () => {
-      console.log('组件卸载')
-    }
-  },[])
+  useEffect(() => {
+    // console.log('count发生了变化', count)
+    // return () => {
+    //   console.log('组件卸载')
+    // }
+  },[count])
 
+  useEffect(() => {
+    // console.log('count或name发生了变化', count)
+    // return () => {
+    //   console.log('组件卸载')
+    // }
+  },[count, name])
+
+
+  function btn_click() {
+    setCount((count) => {
+      return count + 1
+    })
+    // count是几？
+    // console.log(count)
+  }
+
+  function btn_click2() {
+    setName('李四')
+  }
 
   return (
-    <h1>hello hooks</h1>
+    <div>
+      <h1>{count}</h1>
+      <h1>{name}</h1>
+      <button onClick={btn_click}>点我+1</button>
+      <button onClick={btn_click2}>点我修改名字</button>
+    </div>
   )
 }
 
