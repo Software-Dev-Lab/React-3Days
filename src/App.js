593d5c46ddd0ab1baff0729f268630904a474c2e
9j <azer6666@163.com>
2024-04-06 16:16:23 +0800

    十九课： useid

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { useState, createContext, useContext, useDebugValue, useEffect } from 'react';
+import React, { useState, createContext, useContext, useDebugValue, useEffect, useId } from 'react';
 
-function Bpp() {
-  useDebugValue('xxxx', 'xxxxx')
+function PasswordField() {
+  const passwordHintId = useId();
+  console.log('passwordHintId', passwordHintId)
+
 
   return (
-    <h1>
-      bpp
-    </h1>
-  )
-
+    <>
+      <label>
+        密码:
+        <input
+          type="password"
+          aria-describedby={passwordHintId}
+        />
+      </label>
+      <p id={passwordHintId}>
+        密码应该包含至少 18 个字符
+      </p>
+    </>
+  );
 }
 
-function App () {
+function App() {
 
+  const id = useId()
 
-  const [count, setCount] = useState(0);
+  console.log('id', id)
 
-  console.log('count', count)
-  useDebugValue(count)
 
   return (
-    <h1>
-      app use debug value
-    </h1>
+    <div>
+      <h2>输入密码</h2>
+      <PasswordField />
+      <h2>确认密码</h2>
+      <PasswordField />
+
+    </div>
   )
 }
 
